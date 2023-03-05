9c63373e4643f179d858a3241455c153638eefeb
KhabarovaND <N.D.Khabarova@gmail.com>
2023-03-06 02:07:21 +0300

    задача38

---
+++ data_create.py
@@ -0,0 +1,16 @@
+
+def name_data():
+    name = input("введите имя: ")
+    return name
+
+def surname_data():
+    surname = input("введите фамилию: ")
+    return surname
+
+def phone_data():
+    phone = input("введите номер телефона: ")
+    return phone
+
+def adress_data():
+    adress = input("введите адрес: ")
+    return adress
